972c27311acb7f485d3cfb045f670a3b67c2953f
Marc Osterland <marc.osterland@gmail.com>
2023-02-26 21:37:32 +0100

    Explain Step Size

--- Firmware/main.py
+++ Firmware/main.py
@@ -10,11 +10,17 @@ enable.off()
 led.off()
 direction.on()
 
+motor_stepsize = 1.8 # degrees
+microstepping = 16
+step_size_eff = motor_stepsize/microstepping
+hrs_per_rev = 24 # earth revolves ones a day
+step_interval = (3600*hrs_per_rev) / (360/step_size_eff)
+
 def stepOne(s=0.01):
   step.on()
-  utime.sleep(s)
+  utime.sleep(s/2)
   step.off()
-  utime.sleep(s)
+  utime.sleep(s/2)
 
 def stepn(n, s=0.01):
     for i in range(n):
@@ -32,6 +38,6 @@ stepn(500, 0.01)
 direction.on()
 
 while True:
-    stepn(324000, (2/15))
+    stepn(324000, step_interval)
 
 
